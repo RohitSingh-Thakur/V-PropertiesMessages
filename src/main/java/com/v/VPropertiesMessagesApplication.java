42e02427ac7ca07211050bd9e8b9e29539c224e0
RohitSingh <thakurrohitdsingh@gmail.com>
2024-06-01 14:39:03 +0530

    first commit

---
+++ src/main/java/com/v/VPropertiesMessagesApplication.java
@@ -0,0 +1,33 @@
+package com.v;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@SpringBootApplication
+public class VPropertiesMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VPropertiesMessagesApplication.class, args);
+	}
+	
+	
+	@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:fieldValidationMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+}
